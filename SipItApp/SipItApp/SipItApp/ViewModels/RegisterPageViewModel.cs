41b829674b5834143e87669431d42741d9a4acf6
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-04-21 16:44:03 -0600

    Login navigation works now

--- SipItApp/SipItApp/SipItApp/ViewModels/RegisterPageViewModel.cs
+++ SipItApp/SipItApp/SipItApp/ViewModels/RegisterPageViewModel.cs
@@ -1,15 +1,65 @@
 ﻿using Prism.Navigation;
+using SipItApp.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using Xamarin.Forms;
 
 namespace SipItApp.ViewModels
 {
     public class RegisterPageViewModel : ViewModelBase
     {
-        public RegisterPageViewModel() 
+        private Customer customer;
+        public RegisterPageViewModel()
         {
-
+            customer = new Customer();
         }
+
+        //DebuggerDisplayAttribute
+
+        private String userFirstName;
+        public String UserFirstName {get; set;}
+
+        private String userLastName;
+        public String UserLastName { get; set; }
+
+        private String username;
+        public String Username { get; set; }
+
+        private String userEmail;
+        public String UserEmail { get; set; }
+
+        private String password;
+        public String Password { get; set; }
+
+        private Command createAccount;
+        public Command CreateAccount => createAccount ?? (createAccount = new Command(async
+            () =>
+            {
+                Debug.WriteLine("CreateAccount command");             
+
+                Debug.WriteLine(UserFirstName);
+                customer.FirstName = UserFirstName;
+
+                Debug.WriteLine(UserLastName);
+                customer.LastName = UserLastName;
+
+                Debug.WriteLine(UserEmail);
+                customer.Email = UserEmail;
+
+                Shell.Current.SendBackButtonPressed();
+                Shell.Current.SendBackButtonPressed();
+            }));
+
+        private Command goToLoginPage;
+        public Command GoToLoginPage => goToLoginPage ?? (goToLoginPage = new Command(async 
+            () =>
+            {
+                Debug.WriteLine("GoToLoginPage command was triggered");
+                //await Shell.Current.GoToAsync("login", true);
+                Shell.Current.SendBackButtonPressed();
+
+            }));        
     }
 }
--- SipItApp/SipItApp/SipItApp/Views/RegisterPage.xaml.cs
+++ SipItApp/SipItApp/SipItApp/Views/RegisterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SipItApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace SipItApp.Views
         public RegisterPage()
         {
             InitializeComponent();
+            BindingContext = Startup.ServiceProvider.GetService<RegisterPageViewModel>();
         }
     }
 }
